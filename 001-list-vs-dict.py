53f6015d9378ef40fbe2849fe5e1015b870ab88a
Emad <sudani.emad@gmail.com>
2024-01-14 19:57:14 +0330

    add list-vs-dict example

---
+++ 001-list-vs-dict.py
@@ -0,0 +1,16 @@
+import time
+
+# lst = list(range(100_000_000))
+# t0 = time.perf_counter()
+# print(99_999_999 in lst)
+# t1 = time.perf_counter()
+
+# print(t1 - t0)
+
+# dct = {i: i for i in list(range(100_000_000))}
+# t0 = time.perf_counter()
+# print(99_999_999 in dct)
+# t1 = time.perf_counter()
+# print(t1 - t0)
+
+print(dir(time))
