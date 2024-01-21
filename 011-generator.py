dd59575c5b93337895f6b0eed9727b9fef024f90
Emad <sudani.emad@gmail.com>
2024-01-21 20:57:06 +0330

    add generator file

---
+++ 011-generator.py
@@ -0,0 +1,17 @@
+def odds(max):
+    num = 1
+
+    while num <= max:
+        yield num
+        num += 2
+
+for i in odds(10):
+    print(i)
+
+# gen = odds(10)
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
