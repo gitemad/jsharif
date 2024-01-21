979ffdac2b4f676cf3610fa9e09cf85ac6c05ca1
Emad <sudani.emad@gmail.com>
2024-01-21 20:31:53 +0330

    add modular programming

---
+++ 006-modular-programming.py
@@ -0,0 +1,19 @@
+# import math
+# print(math.pi)
+
+# from math import pi, sin
+# print(pi)
+# print(sin(pi))
+
+# from math import *
+# print(pi)
+# print(sin(pi))
+# print(exp(1))
+
+# from math import degrees as deg, pi
+# import numpy as np
+
+# print(deg(pi))
+
+import module
+module.f()
