29731a19a9f3bc3e38e7fbb0470e52a2946cc13b
Emad <sudani.emad@gmail.com>
2024-01-23 18:47:20 +0330

    add multithreading file

---
+++ 016-multithreading.py
@@ -0,0 +1,61 @@
+# import requests
+# import time
+
+# def fetch_data(url):
+#     return requests.get(url)
+
+# if __name__ == '__main__':
+#     slug_list = [
+#         "If-we-quantify-the-alarm-we-can-get-to-the-FTP-pixel-through-the-online-SSL-interface!-120863",
+#         "Try-to-transmit-the-HTTP-card-maybe-it-will-override-the-multi-byte-hard-drive!-120863",
+#         "Try-to-generate-the-TCP-bus-maybe-it-will-override-the-neural-bandwidth!-120863",
+#         "You-cant-transmit-the-firewall-without-copying-the-1080p-SDD-interface!-120863",
+#         "You-cant-connect-the-interface-without-programming-the-virtual-PNG-protocol!-120863",
+#         "Try-to-bypass-the-SAS-card-maybe-it-will-transmit-the-solid-state-system!-120863",
+#         "Use-the-auxiliary-EXE-monitor-then-you-can-hack-the-haptic-port!-120863",
+#     ]
+#     time0 = time.perf_counter()
+
+#     for slug in slug_list:
+#         fetch_data(f'https://api.realworld.io/api/articles/{slug}')
+    
+#     time1 = time.perf_counter()
+
+#     print(time1 - time0)
+
+
+import threading
+import requests
+import time
+
+def fetch_data(url):
+    return requests.get(url)
+
+if __name__ == '__main__':
+    slug_list = [
+        "If-we-quantify-the-alarm-we-can-get-to-the-FTP-pixel-through-the-online-SSL-interface!-120863",
+        "Try-to-transmit-the-HTTP-card-maybe-it-will-override-the-multi-byte-hard-drive!-120863",
+        "Try-to-generate-the-TCP-bus-maybe-it-will-override-the-neural-bandwidth!-120863",
+        "You-cant-transmit-the-firewall-without-copying-the-1080p-SDD-interface!-120863",
+        "You-cant-connect-the-interface-without-programming-the-virtual-PNG-protocol!-120863",
+        "Try-to-bypass-the-SAS-card-maybe-it-will-transmit-the-solid-state-system!-120863",
+        "Use-the-auxiliary-EXE-monitor-then-you-can-hack-the-haptic-port!-120863",
+    ]
+    time0 = time.perf_counter()
+
+    threads = []
+
+    for slug in slug_list:
+        url = f'https://api.realworld.io/api/articles/{slug}'
+        thread = threading.Thread(target=fetch_data, args=(url,))
+        threads.append(thread)
+    
+    for thread in threads:
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
+    
+    time1 = time.perf_counter()
+
+    print(time1 - time0)
